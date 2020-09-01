f488a020c43bee7f2fc8caee6116946145af6381
Brian Acosta <brian.acosta@xing.com>
2020-09-01 12:49:45 +0200

    Creating options and dropdowns coming from api and maping

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle,WA");
+  //this const is an array bc the useState always returns a default value and the updater function
+  const [location, updateLocation] = useState("Seattle,WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("rottweiler");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -12,8 +17,41 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
-            onChange={(event) => setLocation(event.target.value)}
+            onChange={(event) => updateLocation(event.target.value)}
           ></input>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={!breeds.length} //or breeds.length === 0
+          >
+            <option>ALL</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
