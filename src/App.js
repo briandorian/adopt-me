466379c5be0b3b139d02725998902ef640be9097
Brian Acosta <brian.acosta@xing.com>
2020-09-01 11:16:49 +0200

    Using hooks first time, useState to a searchEngine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div id="something important">
-      <h1>Adopt me!</h1>
-      <Pet name="Kira" animal="Dog" breed="Rottweiler" />
-      <Pet name="Isidoro" animal="Cat" breed="Persian" />
-      <Pet name="Curro" animal="Bird" breed="Mockingjay" />
+      <h1 id="something important">Adopt Me!</h1>
+      <SearchParams />
     </div>
   );
 };
