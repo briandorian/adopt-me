f53117ae558db23032c671d157c0ee1e09338c1b
Brian Acosta <brian.acosta@xing.com>
2020-09-02 09:33:03 +0200

    Adding routing to go back from details page to main page clickin on title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
 //import Pet from "./Pet";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     //React.strictMode does not allow you to use stuff that is about to be deprecated
     <React.StrictMode>
       <div id="something important">
-        <h1 id="something important">Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
---
+++ src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = (props) => {
+  return <cod>{JSON.stringify(props, null, 4)}</cod>;
+};
+
+export default Details;
