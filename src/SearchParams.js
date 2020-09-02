532a627464d62fa44c01946bd00f9dc178c9c6d5
Brian Acosta <brian.acosta@xing.com>
2020-09-02 15:56:23 +0200

    Adding a theme and changing the color of a button across pages

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import Results from "./Results.js";
 import useDropdown from "./useDropdown";
 import { async } from "q";
 import { func } from "prop-types";
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
   //this const is an array bc the useState always returns a default value and the updater function
@@ -12,6 +13,7 @@ const SearchParams = () => {
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   const [pets, setPets] = useState([]);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -52,7 +54,20 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown></AnimalDropdown>
         <BreedDropdown></BreedDropdown>
-        <button>Submit</button>
+        <label htmlFor="location">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets}></Results>
     </div>
